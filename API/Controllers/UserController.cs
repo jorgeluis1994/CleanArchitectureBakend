2073a45b52bfe0e0a5fecc558519bfcfea769c3b
Jorge_Luis <luisortiz211994@gmai.com>
2025-01-09 21:04:13 -0500

    mongo

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,20 +1,36 @@
+using API.Db;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace MyApp.Namespace
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
-    {
-        // Simulamos una base de datos con una lista en memoria
-        private static List<string> users = new List<string> { "John Doe", "Jane Smith" };
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IMongoCollection<Object> _collection;
 
-        // Acción GET
-        [HttpGet]
-        public IActionResult GetUsers()
+    public UserController(IMongoClient mongoClient, IOptions<MongoDbSettings> settings)
+    {
+        var database = mongoClient.GetDatabase(settings.Value.DatabaseName);
+        _collection = database.GetCollection<Object>("users");
+    }
+
+    // Acción GET
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var usuarios = await _collection.Find(_ => true).ToListAsync();
+        
+        if (usuarios == null || usuarios.Count == 0)
         {
-            return Ok(users);  // Devuelve la lista de usuarios
+            return NotFound("No users found.");
         }
+        
+        return Ok(usuarios);
     }
 }
+}
+
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,23 @@
+using API.Db;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers(); 
+
+
+// Configurar MongoDbSettings desde appsettings.json
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+// Configurar la conexi�n de MongoDB
+builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -11,12 +27,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers(); 
 
 app.Run();
 
